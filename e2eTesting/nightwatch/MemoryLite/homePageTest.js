3724e8065ac0713aede2e78f9fb3850b9df13f9b
IsabellaMclean <80723693+Isabella-Mclean@users.noreply.github.com>
2026-02-27 02:32:33 +0000

    Setup repo with initial tests

---
+++ e2eTesting/nightwatch/MemoryLite/homePageTest.js
@@ -0,0 +1,27 @@
+
+describe('HomePage Assertion', function() {
+    
+  beforeEach(function (browser) {
+
+    browser.navigateTo('https://apps.zappdev.io/MemoryLite_906_isabellamftr/HomePage/Render');
+  });
+
+
+  it('tests title of the page', function(browser) {
+    browser.assert.titleEquals('Home Page | MemoryLite');
+  });
+
+  it('test sign in button redirects to sign in page', function(browser) {
+    //click sign in button
+    browser.click('a[href*="SignInPage/Load"]');
+
+    //assert username and password input fields are present and url is sign in
+    browser.assert.elementPresent('input[type=text]');
+    browser.assert.elementPresent('input[type=password]');
+    browser.assert.urlContains('SignInPage/Load');
+  });
+
+  afterEach(function (browser) {
+    browser.end();
+  });
+});
