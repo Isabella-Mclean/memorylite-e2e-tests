95989db28a7d723e1e03fd934f5c166a5ec15b48
IsabellaMclean <80723693+Isabella-Mclean@users.noreply.github.com>
2026-03-03 13:36:05 +0000

    Fix dependencies

--- e2eTesting/nightwatch.conf.js
+++ e2eTesting/nightwatch.conf.js
@@ -11,6 +11,8 @@
 //             __/ |
 //            |___/
 
+require('dotenv').config();
+
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
