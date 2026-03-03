d06ec269740184e4e2c4fb3879bfe01a9972e973
IsabellaMclean <80723693+Isabella-Mclean@users.noreply.github.com>
2026-03-03 17:17:40 +0000

    Remove test skips

--- e2eTesting/MemoryLite/signInPageTest.js
+++ e2eTesting/MemoryLite/signInPageTest.js
@@ -6,7 +6,7 @@ describe('SignInPage Assertions', function() {
     browser.navigateTo('https://apps.zappdev.io/MemoryLite_906_isabellamftr/HomePage/Render');
   });
 
-  it.skip('test login successful with valid credentials redirects to home page', function(browser) {
+  it('test login successful with valid credentials redirects to home page', function(browser) {
     //click sign in button
     browser.click('a[href*="SignInPage/Load"]');
     let usernameInput = 'input[type=text]';
@@ -22,7 +22,7 @@ describe('SignInPage Assertions', function() {
     browser.assert.urlContains('UserHome/Index');
   });
 
-  it.skip('test login unsuccessful with invalid credentials', function(browser) {
+  it('test login unsuccessful with invalid credentials', function(browser) {
     //click sign in button
     browser.click('a[href*="SignInPage/Load"]');
     let usernameInput = 'input[type=text]';
@@ -38,7 +38,7 @@ describe('SignInPage Assertions', function() {
     browser.assert.textContains('body','Sign In failed. Please check your credentials.');
   });
   
-  it.skip('test unauthorised users on the userhome page are redirected to the sign in page', function(browser) {
+  it('test unauthorised users on the userhome page are redirected to the sign in page', function(browser) {
     //navigate straight to the user home
     browser.navigateTo('https://apps.zappdev.io/MemoryLite_906_isabellamftr/UserHome/Index');
 
@@ -46,7 +46,7 @@ describe('SignInPage Assertions', function() {
     browser.assert.urlContains('SignInPage/Load');
   });
 
-  it.skip('test unauthorised users on the add entry page are redirected to the sign in page', function(browser) {
+  it('test unauthorised users on the add entry page are redirected to the sign in page', function(browser) {
     //navigate straight to the add entry page
     browser.navigateTo('https://apps.zappdev.io/MemoryLite_906_isabellamftr/AddEntry/Index');
 
